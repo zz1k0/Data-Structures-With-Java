d19e00baa9a10b026d6e9ed749d3345eaea0b05d
Zakariya Bilal <33417767+zz1k0@users.noreply.github.com>
2022-01-22 15:54:51 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,131 +1,165 @@
+import java.util.NoSuchElementException;
+
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 
 class Main {
   public static void main(String[] args) {
-     
-     var list = new LinkedList();
-     
-     list.addLast(10);
-     list.addLast(20);
-     list.addLast(30);
 
-     list.addFirst(9);
-    //  System.out.println(list.contains(9));
+    var list = new LinkedList();
 
-    //  list.removeFirst();
+    list.addLast(10);
+    list.addLast(20);
+    list.addLast(30);
 
-    list.removeLast();
-    // System.out.println(list.size());
-    
+    list.addFirst(9);
+    // System.out.println(list.contains(9));
+
+    // list.removeFirst();
+
+    list.removeLastV2();
+   System.out.println(list.size());
+
   }
 }
 
+class LinkedList {
 
-class LinkedList{
-  
-  private class Node{
-      private int value  ;
-      private Node next; 
-      
-      public Node(int value){
-          this.value = value  ;
-      }
-  }
-  
-  private int size = 0 ; 
-  private Node first ; 
-  private Node last ; 
-  
-  //addLast  
-  
-  public void addLast(int item){
-      var node  = new Node(item);
-      
-      if(isEmpty())
-       first = last = node ; 
-      else {
-      last.next = node ; 
-      last = node ; 
-      }
-      size++;
-  } 
-  
-  
-  public void addFirst(int item){
-      var node = new Node(item);
-      if(isEmpty())
-        first = last = node ; 
-      else{
-          node.next = first;
-          first = node ; 
-      }
+  private class Node {
+    private int value;
+    private Node next;
 
-      size++;
+    public Node(int value) {
+      this.value = value;
+    }
   }
-  
-  public int indexOf(int item){
-      int index=  0 ;
-      var current  = first ; 
-      while(current != null){
-          if(current.value == item) return index; 
-          current = current.next; 
-          index++;
-      }
-      return -1 ;
+
+  private int size = 0;
+  private Node first;
+  private Node last;
+
+  // addLast
+
+  public void addLast(int item) {
+    var node = new Node(item);
+
+    if (isEmpty())
+      first = last = node;
+    else {
+      last.next = node;
+      last = node;
+    }
+    size++;
   }
-  
-  
-  //remote first 
-  public void removeFirst(){
-    
-    first = first.next ; 
+
+  public void addFirst(int item) {
+    var node = new Node(item);
+    if (isEmpty())
+      first = last = node;
+    else {
+      node.next = first;
+      first = node;
+    }
+
+    size++;
+  }
+
+  public int indexOf(int item) {
+    int index = 0;
+    var current = first;
+    while (current != null) {
+      if (current.value == item)
+        return index;
+      current = current.next;
+      index++;
+    }
+    return -1;
+  }
+
+  // remote first version 1
+  public void removeFirstV1() {
+
+    if (isEmpty())
+      throw new NoSuchElementException();
+
+    if (first == last) {
+      first = last = null;
+      return;
+    } else {
+      var second = first.next;
+      first.next = null;
+      first = second;
+    }
+
     size--;
-    
+
   }
-  
-  
-  //remove last 
-  public void removeLast(){
-    
-    int index = 0 ;
-    var current  = first ;
-    int lastIndex =  indexOf(last.value);
-    
-    while(current != null){
-      
-      if(index == (lastIndex-1)){
-        last = current ; 
+
+  // remove last
+  public void removeLastV1() {
+
+    int index = 0;
+    var current = first;
+    int lastIndex = indexOf(last.value);
+
+    while (current != null) {
+      if (index == (lastIndex - 1)) {
+        last = current;
         last.next = null;
         break;
       }
-      
-      current = current.next; 
+      current = current.next;
       index++;
-      
-    }    
+    }
     size--;
   }
-  
-  //contain
-  public boolean contains(int item){
+
+  // remove last
+  public void removeLastV2() {
+
+    if (isEmpty())
+      throw new NoSuchElementException();
+
+    if (first == last)
+      first = last = null;
+    else {
+      var previous = getPreviousLast(last);
+      last = previous;
+      last.next = null;
+    }
+
+    size--;
+  }
+
+  // contain
+  public boolean contains(int item) {
     return indexOf(item) != -1;
   }
-  
-  public int size(){
+
+  public int size() {
     return size;
   }
 
-
-  //implementation detail
-  private boolean isEmpty(){
-      return first == null; 
-  }
-
-
-
-  public void reverse(){
+  public void reverse() {
 
   }
-  
+
+  // implementation detail
+  private boolean isEmpty() {
+    return first == null;
+  }
+
+  // get the item before the last one
+  private Node getPreviousLast(Node node) {
+
+    var current = first;
+
+    while (current != null) {
+      if (current.next == node)
+        return current;
+      current = current.next;
+    }
+    return null;
+  }
+
 }
